c7d711db6966a7864ed548465c6ca41be3f45a8d
zahid <md.zahid.md.ilyas>
2024-09-04 09:37:13 +0530

    add two number

--- index.js
+++ index.js
@@ -5,7 +5,4 @@ const name = 'zahid';
 const dob = '01/01/2024'
 const is adult = () => {
     return age >= 18;
-    return true;
-    else
-return false;
 }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function myfunk() {
     console.log("this is a function")
-    
+    console.log("this is thge second function")   
 }
